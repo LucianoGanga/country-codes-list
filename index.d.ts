0358f8826f36d61310577c87f725b7c47bf86641
フランソワ <Francois_vd@icloud.com>
2023-03-02 11:51:17 +0100

    feat: exports CountryProperty enum & update customArray signature

--- index.d.ts
+++ index.d.ts
@@ -2,7 +2,7 @@
 // by jakeisnt, Feb. 2022
 
 declare module 'country-codes-list' {
-  enum CountryProperty {
+  export enum CountryProperty {
     countryNameEn = 'countryNameEn',
     countryNameLocal = 'countryNameLocal',
     countryCode = 'countryCode',
@@ -19,43 +19,49 @@ declare module 'country-codes-list' {
   }
 
   type CountryData = {
-    [CountryProperty.countryNameEn]: string,
-    [CountryProperty.countryNameLocal]: string,
-    [CountryProperty.countryCode]: string,
-    [CountryProperty.currencyCode]: string,
-    [CountryProperty.currencyNameEn]: string,
-    [CountryProperty.tinType]: string,
-    [CountryProperty.tinName]: string,
-    [CountryProperty.officialLanguageCode]: string,
-    [CountryProperty.officialLanguageNameEn]: string,
-    [CountryProperty.officialLanguageNameLocal]: string,
-    [CountryProperty.countryCallingCode]: string,
-    [CountryProperty.region]: string,
-    [CountryProperty.flag]: string,
-  };
+    [CountryProperty.countryNameEn]: string
+    [CountryProperty.countryNameLocal]: string
+    [CountryProperty.countryCode]: string
+    [CountryProperty.currencyCode]: string
+    [CountryProperty.currencyNameEn]: string
+    [CountryProperty.tinType]: string
+    [CountryProperty.tinName]: string
+    [CountryProperty.officialLanguageCode]: string
+    [CountryProperty.officialLanguageNameEn]: string
+    [CountryProperty.officialLanguageNameLocal]: string
+    [CountryProperty.countryCallingCode]: string
+    [CountryProperty.region]: string
+    [CountryProperty.flag]: string
+  }
 
-  type Filter<T> = (element: T, index: number, array: T[]) => T[];
+  type Filter<T> = (element: T, index: number, array: T[]) => T[]
 
   type CustomArraySettings = {
-    sortDataBy?: CountryProperty,
-    sortBy?: any,
-    filter?: Filter<CountryData>,
-  };
+    sortDataBy?: CountryProperty
+    sortBy?: any
+    filter?: Filter<CountryData>
+  }
 
-  export function all(): CountryData[];
+  export function all(): CountryData[]
 
-  export function filter(countryProperty: CountryProperty, value: string): CountryData[];
+  export function filter(
+    countryProperty: CountryProperty,
+    value: string,
+  ): CountryData[]
 
-  export function findOne(countryProperty: CountryProperty, value: string): CountryData;
+  export function findOne(
+    countryProperty: CountryProperty,
+    value: string,
+  ): CountryData
 
   export function customArray(
-    fields?: { name?: string, value?: string },
-    settings?: CustomArraySettings
-  ): string[];
+    fields?: Record<string, string>,
+    settings?: CustomArraySettings,
+  ): Record<string, string>[]
 
   export function customList(
     key?: CountryProperty,
     label?: string,
     settings?: CustomArraySettings,
-  ): { [CountryProperty]: string };
+  ): { [key in CountryProperty]: string }
 }
